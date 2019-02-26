cf3b0e9a646d1ef05d625045952c279446958e44
Anders Svendal <anders.svendal@gmail.com>
2019-02-26 11:28:33 +0100

    Add isPresent test to check for not undefined and not null

---
+++ test/is_present.js
@@ -0,0 +1,13 @@
+import test from 'ava';
+
+import isPresent from '../lib/is_present';
+
+test('isPresent', (t) => {
+  t.is(isPresent(undefined), false);
+  t.is(isPresent(null), false);
+
+  t.is(isPresent('string'), true);
+  t.is(isPresent(1), true);
+  t.is(isPresent({}), true);
+  t.is(isPresent([]), true);
+});
